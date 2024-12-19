192982d0d82622d3b533181d7c6aee44cc0fc427
Marta Sanz Florez <marta.sanzflores@kanbios.fr>
2024-12-19 18:25:01 +0100

    mongo congif

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 import AppDataSource from './config/typeorm';
 import bodyParser from 'body-parser';
 import { CustomError } from './types/error';
+import mongoose from 'mongoose';
 
 import authRoutes from './routes/authRoutes';
 import adminRoutes from './routes/adminRoutes';
@@ -29,11 +30,24 @@ app.use((error: CustomError, req: Request, res: Response, next: NextFunction) =>
   res.status(status).json({ message, data });
 });
 
-AppDataSource.initialize()
+// Connexion à MongoDB avec Mongoose
+const mongoUrl = process.env.MONGO_URL || 'mongodb://db:27017/mydatabase'; // L'URL de connexion à MongoDB (elle devrait être définie dans votre .env)
+mongoose.connect(mongoUrl)
   .then(() => {
-    console.log('Database connected!');
-    app.listen(port, () => {
-      console.log('Server running on port 3000');
-    });
+    console.log('MongoDB connected');  // Affichage du message si MongoDB est connecté
+
+    // Connexion à PostgreSQL avec TypeORM
+    AppDataSource.initialize()
+      .then(() => {
+        console.log('PostgreSQL connected!');
+        // Lancer le serveur une fois que les connexions sont réussies
+        app.listen(port, () => {
+          console.log('Server running on port 3000');
+        });
+      })
+      .catch((error) => console.log('Database connection failed: ', error));
   })
-  .catch((error) => console.log('Database connection failed: ', error));
+  .catch((error) => {
+    console.error('MongoDB connection failed: ', error);
+    process.exit(1); // Quitte le processus en cas d'échec de la connexion à MongoDB
+  });
